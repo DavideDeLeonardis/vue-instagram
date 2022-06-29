3cfe0c583162da7fa0420a6cae5f069048f2c3bf
DavideDeLeonardis <davidedeleonardis1711@gmail.com>
2022-06-29 17:33:13 +0200

    add possibility of showing storie

--- src/components/Main/Storie.vue
+++ src/components/Main/Storie.vue
@@ -1,9 +1,18 @@
 <template>
    <li class="storie">
       <img
+         class="img-storie"
          :src="storie.profile_picture"
          :alt="`${storie.profile_name} image`"
+         @click="varShowStorie = true"
       /><br />
+      <div class="container-open-storie" v-if="varShowStorie">
+         <span @click="varShowStorie = false">X</span>
+         <img
+            :src="storie.profile_picture"
+            :alt="`${storie.profile_name} image`"
+         />
+      </div>
       <span>{{ storie.profile_name }}</span>
    </li>
 </template>
@@ -11,6 +20,11 @@
 <script>
 export default {
    name: "Storie",
+   data() {
+      return {
+         varShowStorie: false,
+      };
+   },
    props: {
       storie: {
          type: Object,
